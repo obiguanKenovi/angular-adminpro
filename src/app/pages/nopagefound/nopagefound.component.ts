27d8269fd76a71b79d8fffe693659929107fedd2
José Gonzalez Matos <joseteam2015@outlook.com>
2021-08-31 12:55:43 +0200

    fin primera parate proyecto 01

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.css']
+})
+export class NopagefoundComponent {
+
+  public date:number;
+  constructor() {
+    this.date=new Date().getFullYear();
+   }
+
+ 
+}
